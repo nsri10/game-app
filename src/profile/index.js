20a36fce1ac2bbb8910b42c9a4c2b17d47e12f4e
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-12-11 13:45:14 -0500

    resolve merge

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,15 +1,52 @@
-import NavigationBar from "../nav";
-
+import * as client from "../users/client";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./profile.css";
 
-function Profile() {
-    return (
-        <div className="d-flex profile-page">
-            <NavigationBar />
-            <div>
-                <input type="text" className="search-bar mt-3 ms-3" placeholder="Search for Games"/>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
+function ProfileStatus({ account, isLoggedIn }) {
+    console.log(isLoggedIn);
+    if (isLoggedIn) {
+        return (
+            <div>
+                {account && (
+                <div>
+                    <h1>Username</h1>
+                    <p>
+                        Email <br/>
+                        Phone <br/>
+                    </p>
+                </div>
+                )}
             </div>
+        );
+    } else {
+        return (
+            <h1>Log in to see profile</h1>
+        );
+    }
+}
+
+function Profile() {
+    const [account, setAccount] = useState(null);
+    const navigate = useNavigate();
+    const fetchAccount = async () => {
+        const account = await client.account();
+        setAccount(account);
+    };
+    useEffect(() => {
+        fetchAccount();
+    }, []);
+    const isLoggedIn = account != null;
+    return (
+        <div className="d-flex profile mt-5 ms-5">
+            <FontAwesomeIcon icon={faUserCircle} className="profile-icon mb-3"/>
+            <ProfileStatus
+                account={account}
+                isLoggedIn={isLoggedIn}
+            />
         </div>
     )
 }
---
+++ src/users/client.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const USERS_API = "http://localhost:4000/api/users"; // TODO: replace with env var
+export const createUser = async (user) => {
+    const response = await axios.post(`${USERS_API}`, user);
+    return response.data;
+};  
+export const deleteUser = async (user) => {
+    const response = await axios.delete(`${USERS_API}/${user._id}`);
+    return response.data;
+};  
+export const signup = async (credentials) => {
+    const response = await axios.post(`${USERS_API}/signup`, credentials);
+    return response.data;
+};  
+export const signin = async (credentials) => {
+    const response = await axios.post( `${USERS_API}/signin`, credentials );
+    return response.data;
+};
+export const signout = async () => {
+    const response = await axios.post(`${USERS_API}/signout`);
+    return response.data;
+};  
+export const account = async () => {
+    const response = await axios.post(`${USERS_API}/account`);
+    return response.data;
+};
+  
