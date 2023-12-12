232ab26b73d32bf8bc6d6356526a12beccbd53fd
Neha Sriram <nehasriram10@gmail.com>
2023-12-11 21:38:24 -0500

    anon home page placeholders complete

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,22 @@
 import "./home.css";
+import FeaturedGames from "./featgame";
+import LatestReleases from "./latestrelease";
+import TrendingDiscussions from "./trendingdisc";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div className="d-flex">
-            <div className="home-text mt-5 ms-5">
-                <h1>Featured Games</h1>
-                <h1>Latest Releases</h1>
-                <h1>Trending Discussions</h1>
+        <div className="d-flex" style={{width: '100%'}}>
+            <div className="mt-2 ms-5">
+                <h1 className="home-text">Featured Games</h1>
+                <br />
+                <FeaturedGames />
+                <br />
+                <h1 className="home-text">Latest Releases <span className="home-text4">View all games.</span> </h1> 
+                
+                <LatestReleases />
+                <h1 className="home-text">Trending Discussions <span className="home-text4">View all discussions.</span></h1>
+                <TrendingDiscussions />
                 
             </div>
         </div>
---
+++ src/home/latestrelease.js
@@ -0,0 +1,35 @@
+
+import "./home.css";
+import { React } from "react";
+import { Card } from 'react-bootstrap';
+import SideScroller from "./sidescroll";
+// make each card a link to the game page
+function LatestReleases() {
+    const releases = [
+        { title: 'game 1', desc: "blah blah", image: "link" },
+        { title: 'game 2', desc: "blah blah", image: "link" },
+        { title: 'game 3', desc: "blah blah", image: "link" },
+        { title: 'game 4', desc: "blah blah", image: "link" },
+        { title: 'game 5', desc: "blah blah", image: "link" },
+      ];
+    
+      return (
+        <SideScroller>
+          {releases.map((item, index) => (
+            <span key={index}>
+              <Card className="cardStyle">
+                <Card.Img style={{objectFit: 'cover'}} variant="top" src="https://www.touchtapplay.com/wp-content/uploads/2021/12/Genshin-Impact-2.4-Featured-2.jpg?fit=640%2C347"/>
+                <Card.Body>
+                  <Card.Title className="home-text2">Genshin Impact</Card.Title>
+                  <Card.Text className="home-text3">
+                  Genshin Impact is an open-world, action role-playing game that allows the player to control one of four interchangeable characters in a party.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </span>
+          ))}
+        </SideScroller>
+      );
+    };
+    
+    export default LatestReleases;
