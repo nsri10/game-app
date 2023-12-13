c9446c8777c591ddadc0c810f144617a6c7847dd
jay <78568609+faunya@users.noreply.github.com>
2023-12-13 17:45:34 -0500

    search page refreshes

--- src/search/index.js
+++ src/search/index.js
@@ -5,13 +5,21 @@ import * as client from "./client";
 import "./index.css"
 
 function Search() {
+    const { searchString } = useParams();
     const [games, setGames] = useState([]);
     const [users, setUsers] = useState([]);
     const [discussions, setDiscussions] = useState([]);
 
     const getGames = async () => {
-        const gotGames = await client.findAllGames(); //100
+        const gotGames = await client.findAllGames();
+
+        if (searchString) {
+            const searchGames = gotGames.filter((r) => r.title.includes(searchString));
+            setGames(searchGames);
+            return;
+        }
         setGames(gotGames);
+        console.log(searchString);
     };
 
     useEffect(() => {
@@ -19,36 +27,44 @@ function Search() {
     }, []);
 
     return (
-        <div>
+        <div className="row" key={ searchString}> 
             <h3>Results</h3>
-            <h1>Games</h1>
-            <div className="list-group" style={{ marginLeft: 25 }}>
-                {games.map((game, index) => (
-                    <Link
-                        key={index}
-                        to={`/details/${game.gameID}`}
-                        className={"list-group-item result"}>
-                        <div key={index} className="game_result">
-                            <img className="review_pfp" src="/imgs/placeholders/674277730124300298.png" /><br />
-                            <h2>{game.title}</h2>
-                        </div>
-                    </Link>
+            <div className="col">
+                <h1>Games ({games.length})</h1>
+                <div className="list-group" style={{ marginLeft: 25 }}>
+                    {games.map((game, index) => (
+                        <Link
+                            key={index}
+                            to={`/details/${game.gameID}`}
+                            className={"list-group-item result"}>
+                            <div key={index} className="game_result">
+                                <img className="review_pfp" src="/imgs/placeholders/674277730124300298.png" /><br />
+                                <h2>{game.title}</h2>
+                            </div>
+                            <hr />
+                        </Link>
 
-                ))}
+                    ))}
+                </div>
+
             </div>
 
-            <h1>Users</h1>
-            <div className="list-group" style={{ marginLeft: 25 }}>
-                {users.map((user, index) => (
-                    <div key={index} className="list-group-item review">
+            <div className="col">
+                <h1>Users</h1>
+                <div className="list-group" style={{ marginLeft: 25 }}>
+                    {users.map((user, index) => (
+                        <div key={index} className="list-group-item review">
 
-                        <div style={{ display: "inline-block", width: "fit-content" }}>
-                            <img className="review_pfp" src="/imgs/placeholders/674277730124300298.png" /><br />
-                            <span>@{user.name}</span>
+                            <div style={{ display: "inline-block", width: "fit-content" }}>
+                                <img className="review_pfp" src="/imgs/placeholders/674277730124300298.png" /><br />
+                                <span>@{user.name}</span>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+
             </div>
+
         </div>
     );
 }
--- src/webpage/index.js
+++ src/webpage/index.js
@@ -7,24 +7,43 @@ import Signup from "../signup";
 
 import "./index.css";
 import Search from "../search";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Webpage() {
+  const [refresh, setRefresh] = useState(true);
+
   return (
     <div className="d-flex detail-page">
       <NavigationBar />
+
       <div style={{ width: "100%" }}>
         <div className="row">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search for Games"
-          />
+          <div>
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search for Games"
+            />
+            <Link className="btn btn-circle btn-primary"
+              to={`/search/stardew`}
+              onClick={() => (setRefresh(!refresh))}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Link>
+
+          </div>
         </div>
+
         <div className="row">
           <Routes>
             <Route path="/" element={<Navigate to="/details/100" />} />
+            <Route path="/search/:searchString" element={<Search key={refresh}/>} />
             <Route path="/search/*" element={<Search />} />
-            <Route path="/details/" element={<Navigate to="/search/"/>} />
+            <Route path="/details/" element={<Navigate to="/search/" />} />
             <Route path="/details/:gameID" element={<Details />} />
             <Route path="/profile/*" element={<Profile />} />
             <Route path="/signup/*" element={<Signup />} />
