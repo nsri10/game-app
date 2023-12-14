1625fc42e508435f223bc11f6c139103dc2c1f79
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-12-13 21:59:47 -0500

    added pfp and bio

--- src/profile/edit_profile/index.js
+++ src/profile/edit_profile/index.js
@@ -23,7 +23,7 @@ function ProfileEditor() {
         <div className="profile mt-5 ms-5">
             {account && (
                 <div>
-                    <h2 className="mb-3">Edit personal data for {account.username}</h2>
+                    <h2 className="mb-3">Edit profile and personal data for {account.username}</h2>
                     <label htmlFor="email" className="form-label mt-2">
                         Email
                     </label>
@@ -32,6 +32,24 @@ function ProfileEditor() {
                         value={account.email}
                         onChange={(e) => setAccount({ ...account,
                             email: e.target.value })}
+                    />
+                    <label htmlFor="pfp" className="form-label mt-2">
+                        Profile Picture
+                    </label>
+                    <input
+                        className="form-control form-input mb-2"
+                        value={account.pfp}
+                        onChange={(e) => setAccount({ ...account,
+                            pfp: e.target.value })}
+                    />
+                    <label htmlFor="bio" className="form-label mt-2">
+                        Bio
+                    </label>
+                    <textarea
+                        className="form-control form-input mb-2"
+                        value={account.bio}
+                        onChange={(e) => setAccount({ ...account,
+                            bio: e.target.value })}
                     />
                     <button onClick={save} className="btn btn-secondary mt-4">
                         Save
--- src/profile/index.js
+++ src/profile/index.js
@@ -6,32 +6,58 @@ import "./profile.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
+function ProfilePicture({ pfp }) {
+    if (pfp) {
+        return (
+            <img src={`/imgs/placeholders/${pfp}`} className="profile-picture ms-2 me-3"/>
+        );
+    } else {
+        return (
+            <FontAwesomeIcon icon={faUserCircle} className="profile-icon ms-2 me-3"/>
+        );
+    }
+}
+
 function ProfileStatus({ account, username }) {
     if (username) {
         return (
-            <div>
+            <div className="d-flex mb-5">
                 {account && (
-                <div>
-                    <h1>{account.username}</h1>
+                <div className="d-flex">
+                    {<ProfilePicture
+                        pfp={account.pfp}
+                    />}
+                    <div>
+                        <h1 className="mb-4">{`@${account.username}`}</h1>
+                        <p className="mb-0 about-header">ABOUT ME</p>
+                        <p className="mt-0">{account.bio}</p>
+                    </div>
                 </div>
                 )}
             </div>
         );
     } else if (account) {
         return (
-            <div>
-                <div className="me-5">
+            <div className="d-flex mb-5">
+                <div className="d-flex">
                     {account && (
-                    <div>
-                        <h1>{account.username}</h1>
-                        <p>
-                        Email: {account.email}<br />
-                        </p>
+                    <div className="d-flex">
+                        {<ProfilePicture
+                            pfp={"ying_pfp.jpeg"}
+                        />}
+                        <div>
+                            <h1 className="mb-0">{`@${account.username}`}</h1>
+                            <p className="mt-1">
+                                {account.email}<br />
+                            </p>
+                            <p className="mb-0 about-header">ABOUT ME</p>
+                            <p className="mt-0">{account.bio}</p>
+                        </div>
                     </div>
                     )}
                     <Link to={`/profile/edit_profile/${account.username}`}>
-                        <button className="btn btn-secondary edit-data mt-0 ms-4">
-                            Edit personal data
+                        <button className="btn btn-secondary edit-data ms-4">
+                            Edit Profile
                         </button>
                     </Link>
                 </div>
@@ -39,7 +65,12 @@ function ProfileStatus({ account, username }) {
         );
     } else {
         return (
-            <h1>Anonymous User</h1>
+            <div className="d-flex">
+                {<ProfilePicture
+                        pfp={null}
+                />}
+                <h1>Anonymous User</h1>
+            </div>
         );
     }
 }
@@ -65,11 +96,13 @@ function Profile() {
     return (
         <div className="profile mt-5 ms-5">
             <div className="d-flex mb-5">
-                <FontAwesomeIcon icon={faUserCircle} className="profile-icon mb-5 ms-2 me-5"/>
                 {<ProfileStatus
                     account={account}
                     username={username}
                 />}
+            </div>
+            <div>
+                <h2>Favorite Games</h2>
             </div>
         </div>
     )
