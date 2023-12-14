19cecbc00e5fdbf74b4c49d1d38a8883300edcee
Neha Sriram <nehasriram10@gmail.com>
2023-12-14 16:46:06 -0500

    link game to details

--- src/home/featgame.js
+++ src/home/featgame.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import "./home.css";
 
 function FeaturedGames() {
@@ -21,12 +22,14 @@ function FeaturedGames() {
     <Carousel>
       {games.map((game) => (
         <Carousel.Item key={game.id} interval={3000}>
+          <Link to={`/details/${game.id}`}>
           <img
             className="featuredCarousel"
             style={{ width: 250, height: 300, objectFit: "cover" }}
             src={`https:${game.cover.url}`}
             alt={game.name}
           />
+          </Link>
           <Carousel.Caption>
             <h3 style={{ marginLeft: 150, width: 400 }}>{game.name}</h3>
           </Carousel.Caption>
