d2072226b1584f2cf4ec9201caf3e0c56193b49b
Eileen Lee <lee.ei@northeastern.edu>
2023-12-14 16:32:36 -0500

    latest releases

--- src/home/featgame.js
+++ src/home/featgame.js
@@ -1,39 +1,39 @@
-
-import React, { useState, useEffect } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import axios from 'axios';
-import './home.css';
+import React, { useState, useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import axios from "axios";
+import "./home.css";
 
 function FeaturedGames() {
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:4000/api/home/featured')
-            .then(response => {
-                setGames(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error fetching the data', error);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/home/featured")
+      .then((response) => {
+        setGames(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data", error);
+      });
+  }, []);
 
-    return (
-        <Carousel>
-            {games.map((game) => (
-                <Carousel.Item key={game.id} interval={3000}>
-                    <img
-                        className="featuredCarousel"
-                        style={{width: 250, height: 300, objectFit: 'cover'}}
-                        src={`https:${game.cover.url}`}
-                        alt={game.name}
-                    />
-                    <Carousel.Caption>
-                        <h3 style={{marginLeft: 150, width: 400}} >{game.name}</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))}
-        </Carousel>
-    );
+  return (
+    <Carousel>
+      {games.map((game) => (
+        <Carousel.Item key={game.id} interval={3000}>
+          <img
+            className="featuredCarousel"
+            style={{ width: 250, height: 300, objectFit: "cover" }}
+            src={`https:${game.cover.url}`}
+            alt={game.name}
+          />
+          <Carousel.Caption>
+            <h3 style={{ marginLeft: 150, width: 400 }}>{game.name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
 
 export default FeaturedGames;
--- src/home/latestrelease.js
+++ src/home/latestrelease.js
@@ -1,35 +1,59 @@
-
 import "./home.css";
 import { React } from "react";
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 import SideScroller from "./sidescroll";
 // make each card a link to the game page
 function LatestReleases() {
-    const releases = [
-        { title: 'game 1', desc: "blah blah", image: "link" },
-        { title: 'game 2', desc: "blah blah", image: "link" },
-        { title: 'game 3', desc: "blah blah", image: "link" },
-        { title: 'game 4', desc: "blah blah", image: "link" },
-        { title: 'game 5', desc: "blah blah", image: "link" },
-      ];
-    
-      return (
-        <SideScroller>
-          {releases.map((item, index) => (
-            <span key={index}>
-              <Card style={{marginBottom: -5, marginTop: -20}} className="cardStyle">
-                <Card.Img style={{objectFit: 'cover'}} variant="top" src="https://www.touchtapplay.com/wp-content/uploads/2021/12/Genshin-Impact-2.4-Featured-2.jpg?fit=640%2C347"/>
-                <Card.Body>
-                  <Card.Title className="home-text2">Genshin Impact</Card.Title>
-                  <Card.Text className="home-text3">
-                  Genshin Impact is an open-world, action role-playing game that allows the player to control one of four interchangeable characters in a party.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </span>
-          ))}
-        </SideScroller>
-      );
-    };
-    
-    export default LatestReleases;
+  const releases = [
+    {
+      title: "Blood on the Clocktower",
+      desc: "Blood on the Clocktower is a game of murder and mystery, lies and logic, deduction and deception for five-to-twenty courageous players and one devious storyteller.",
+      image: "link",
+    },
+    {
+      title: "Cardania: Epoch Wars",
+      desc: "Epoch Wars is a live multiplayer strategy game with hundreds of players from around the world. Battle for strategic resources, explore mysterious anomalies, earn EXP and Victory Points for yourself and one of 7 unique Factions, all vying for control of Cardania! ",
+      image: "link",
+    },
+    {
+      title: "Maxon Petting Simulator",
+      desc: "Maxon Petting Simulator is a clicker game about a cute kitty whose cheeks must always be squished. In this uncomplicated situation, Maxon's friends can help you: the cat who likes to jump by the TV, Manulooshka, the Sandwich Cat, and many others.",
+      image: "link",
+    },
+    {
+      title: "Dragon Ball Z: Kakarot",
+      desc: "Choose your Saiyans , draw your deck, and get ready for Dragon Ball Card Warriors- The in Depth/Fast paced strategy card game that's easy to learn and INCREDIBLY FUN! Start a free game inside of Dragon Ball Z: Kakarot (on Steam) & play your cards to sling Events, summon DBZ Characters, and SPECIAL Character Cards!!! ",
+      image: "link",
+    },
+    {
+      title: "Space Station 13",
+      desc: "Space Station 13 (ss13) is a multiplayer role-playing game. Originally created by one person named Exadv1, development has been continued by members of the fan base.",
+      image: "link",
+    },
+  ];
+
+  return (
+    <SideScroller>
+      {releases.map((item, index) => (
+        <span key={index}>
+          <Card
+            style={{ marginBottom: -5, marginTop: -20 }}
+            className="cardStyle"
+          >
+            <Card.Img
+              style={{ objectFit: "cover" }}
+              variant="top"
+              src="https://www.touchtapplay.com/wp-content/uploads/2021/12/Genshin-Impact-2.4-Featured-2.jpg?fit=640%2C347"
+            />
+            <Card.Body>
+              <Card.Title className="home-text2">{item.title}</Card.Title>
+              <Card.Text className="home-text3">{item.desc}</Card.Text>
+            </Card.Body>
+          </Card>
+        </span>
+      ))}
+    </SideScroller>
+  );
+}
+
+export default LatestReleases;
